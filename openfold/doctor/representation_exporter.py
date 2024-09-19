f9efbe32cd291bf7226ccf0d6c1b5ee2ea49d1f9
Leonardo Giannantoni <leonardo.giannantoni@polito.it>
2024-09-20 00:46:39 +0200

    class for msa and pair representation export

---
+++ openfold/doctor/representation_exporter.py
@@ -0,0 +1,84 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+import torch
+import logging
+
+logger = logging.getLogger(__file__)
+logger.setLevel(level=logging.DEBUG)
+
+class RepresentationExporter:
+    def __init__(self, model, output_dir="heatmaps", export_msa=True, export_pair=True):
+        self.model = model
+        self.output_dir = output_dir
+        self.export_msa = export_msa
+        self.export_pair = export_pair
+        self.iteration = 0
+        os.makedirs(self.output_dir, exist_ok=True)
+        #TODO these should be created only when needed...
+        os.makedirs(os.path.join(self.output_dir, "msa"), exist_ok=True)
+        os.makedirs(os.path.join(self.output_dir, "pair"), exist_ok=True)
+
+        # set callback
+        self.model.representation_hook = self._representation_hook
+
+    def _representation_hook(self, msa_representation, pair_representation, stage, iteration):
+        if self.export_msa:
+            self._save_heatmap(msa_representation, stage, iteration, "msa")
+
+        if self.export_pair:
+            self._save_heatmap(pair_representation, stage, iteration, "pair")
+
+    def _save_heatmap(self, data, stage, iteration, which):
+        if data is None:
+            logger.warning(f"Warning: No data for {title}.")
+            return
+
+        if isinstance(data, torch.Tensor):
+            data = data.detach().cpu().numpy()
+        
+        stage_num = 0 if stage == "before" else 1
+        filename = f"{which}_iter_{iter}_{stage_num}"
+        title = f"{which} representation {stage} recycling {iter}"
+
+        # "squash" the third dimension using average along third axis
+        # N.B. msa shape: (N_alignments, seq_length, msa_dim), pair shape: (seq_length, seq_length, pair_dim)
+        #TODO here we could experiment with PCA, e.g. (just jotted, not tested): 
+        # from sklearn.decomposition import PCA
+        # pca = PCA(n_components=1)
+        # reduced_representation = pca.fit_transform(data.reshape(-1, data.shape[-1])).reshape(data.shape[:-1])
+        reduced_representation = data.mean(axis=-1)
+
+        plt.figure(figsize=(12, 8))
+        sns.heatmap(reduced_representation, cmap='viridis', cbar=True)
+        plt.title(title)
+        plt.xlabel("seq lenght")
+        plt.ylabel("N alignments") if which == "msa" else plt.ylabel("seq length")
+
+        filepath = os.path.join(self.output_dir, f"{filename}.png")
+        plt.savefig(filepath)
+        plt.close()
+        logger.info(f"Heatmap saved: {filepath}")
+
+    def pngs_to_mpg(self, framerate=1):
+        if self.export_msa:
+            self._pngs_to_mpg("msa", framerate)
+        if self.export_pair:
+            self._pngs_to_mpg("pair", framerate)
+
+    def _pngs_to_mpg(self, which, framerate=1):
+        pngs_output_dir = os.path.join(self.output_dir, which)
+        frame_pattern = os.path.join(pngs_output_dir, f"{which}_iter_%01d_%01d.png")
+        output_path = os.path.join(pngs_output_dir, f"{which}_movie.mp4")
+
+        ffmpeg_cmd = [
+            "ffmpeg", "-framerate", str(framerate), "-i", frame_pattern,
+            "-c:v", "libx264", "-pix_fmt", "yuv420p", output_path
+        ]
+
+        try:
+            subprocess.run(ffmpeg_cmd, check=True)
+            logger.info(f"{which} heatmap movie saved: {output_path}")
+        except subprocess.CalledProcessError as e:
+            logger.error(f"Error generating movie: {e}")
+
